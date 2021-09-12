950903c30b22f5ee39f688aa14f9cf97ba74f95c
Petr Osetrov <paradoxfilm2013@gmail.com>
2021-09-12 19:19:41 +0300

    Update InternalConfig.cs

--- src/Dhaf.HealthCheckers.Web/InternalConfig.cs
+++ src/Dhaf.HealthCheckers.Web/InternalConfig.cs
@@ -6,23 +6,22 @@ namespace Dhaf.HealthCheckers.Web
     public class InternalConfig : IHealthCheckerInternalConfig
     {
         public string ExtensionName => "web";
-        public int DefHttpPort { get; set; } = 80;
-        public int DefHttpsPort { get; set; } = 443;
-        public string HttpSchema { get; set; } = "http";
-        public string HttpsSchema { get; set; } = "https";
+        public int DefHttpPort { get; set; }
+        public int DefHttpsPort { get; set; }
+        public string HttpSchema { get; set; }
+        public string HttpsSchema { get; set; }
 
-        public string DefMethod { get; set; } = "GET";
-        public string DefPath { get; set; } = "/";
-        public Dictionary<string, string> DefHeaders { get; set; } = new Dictionary<string, string>();
-        public bool DefFollowRedirects { get; set; } = false;
-        public int DefTimeout { get; set; } = 5 * 1000;
-        public int DefRetries { get; set; } = 2;
+        public string DefMethod { get; set; }
+        public string DefPath { get; set; }
+        public Dictionary<string, string> DefHeaders { get; set; }
+        public bool DefFollowRedirects { get; set; }
+        public int DefTimeout { get; set; }
+        public int DefRetries { get; set; }
 
-        public string DefExpectedCodes { get; set; } = "200";
-        public string ExpectedCodesSeparator { get; set; } = ",";
-        public string ExpectedCodesWildcard { get; set; } = "X";
+        public string DefExpectedCodes { get; set; }
+        public string ExpectedCodesSeparator { get; set; }
+        public string ExpectedCodesWildcard { get; set; }
 
-        public string DefExpectedResponseBody { get; set; } = string.Empty;
-
+        public string DefExpectedResponseBody { get; set; }
     }
 }
