81017442ab14a171a3360e83571b9adf0fd3f248
Petr Osetrov <paradoxfilm2013@gmail.com>
2021-10-07 02:08:58 +0300

    `Etcd` add auth support

--- src/Dhaf.Core/ClusterConfig/Parts/ClusterEtcdConfig.cs
+++ src/Dhaf.Core/ClusterConfig/Parts/ClusterEtcdConfig.cs
@@ -3,6 +3,10 @@
     public class ClusterEtcdConfig
     {
         public string Hosts { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
         public int? LeaderKeyTtl { get; set; }
     }
 }
