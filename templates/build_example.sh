315c47878896e99087f0fefa6575411c8bdb08be
Petr Osetrov <paradoxfilm2013@gmail.com>
2021-10-13 18:29:32 +0300

    Update build_example.sh

--- templates/build_example.sh
+++ templates/build_example.sh
@@ -20,6 +20,7 @@ dotnet publish src/Dhaf.HealthCheckers.Web/Dhaf.HealthCheckers.Web.csproj --conf
 dotnet publish src/Dhaf.HealthCheckers.Exec/Dhaf.HealthCheckers.Exec.csproj --configuration Release --no-restore -nowarn:CS1998 -r $RID -o "bin/$RID/ext/hc/exec" /p:DebugType=None /p:DebugSymbols=false
 
 dotnet publish src/Dhaf.Switchers.Cloudflare/Dhaf.Switchers.Cloudflare.csproj --configuration Release --no-restore -nowarn:CS1998 -r "$RID" -o "bin/$RID/ext/sw/cloudflare" /p:DebugType=None /p:DebugSymbols=false
+dotnet publish src/Dhaf.Switchers.GoogleCloud/Dhaf.Switchers.GoogleCloud.csproj --configuration Release --no-restore -nowarn:CS1998 -r "$RID" -o "bin/$RID/ext/sw/google-cloud" /p:DebugType=None /p:DebugSymbols=false
 dotnet publish src/Dhaf.Switchers.Exec/Dhaf.Switchers.Exec.csproj --configuration Release --no-restore -nowarn:CS1998 -r "$RID" -o "bin/$RID/ext/sw/exec" /p:DebugType=None /p:DebugSymbols=false
 
 dotnet publish src/Dhaf.Notifiers.Email/Dhaf.Notifiers.Email.csproj --configuration Release --no-restore -nowarn:CS1998 -r "$RID" -o "bin/$RID/ext/ntf/email" /p:DebugType=None /p:DebugSymbols=false
@@ -32,9 +33,11 @@ mkdir -p $RID-artifacts/libs
 mkdir -p $RID-artifacts/extensions/health-checkers/web
 mkdir -p $RID-artifacts/extensions/health-checkers/exec
 mkdir -p $RID-artifacts/extensions/switchers/cloudflare
+mkdir -p $RID-artifacts/extensions/switchers/google-cloud
 mkdir -p $RID-artifacts/extensions/switchers/exec
 mkdir -p $RID-artifacts/extensions/notifiers/email
 mkdir -p $RID-artifacts/extensions/notifiers/tg
+
 mv ./bin/$RID/Dhaf.Node $RID-artifacts/dhaf.node
 mv ./bin/$RID/Dhaf.CLI $RID-artifacts/dhaf.cli
 mv ./bin/$RID/appsettings.json ./bin/$RID/nlog.config $RID-artifacts
@@ -42,6 +45,7 @@ mv ./bin/$RID/core/* $RID-artifacts/libs
 mv ./bin/$RID/ext/hc/web/* $RID-artifacts/extensions/health-checkers/web
 mv ./bin/$RID/ext/hc/exec/* $RID-artifacts/extensions/health-checkers/exec
 mv ./bin/$RID/ext/sw/cloudflare/* $RID-artifacts/extensions/switchers/cloudflare
+mv ./bin/$RID/ext/sw/google-cloud/* $RID-artifacts/extensions/switchers/google-cloud
 mv ./bin/$RID/ext/sw/exec/* $RID-artifacts/extensions/switchers/exec
 mv ./bin/$RID/ext/ntf/email/* $RID-artifacts/extensions/notifiers/email
 mv ./bin/$RID/ext/ntf/tg/* $RID-artifacts/extensions/notifiers/tg
