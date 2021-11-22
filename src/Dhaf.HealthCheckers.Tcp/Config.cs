7b9956a07352fbd7f36d365736fe08b67557665b
Petr Osetrov <paradoxfilm2013@gmail.com>
2021-11-23 00:48:28 +0300

    `Dhaf.HealthCheckers` Add TCP hc

---
+++ src/Dhaf.HealthCheckers.Tcp/Config.cs
@@ -0,0 +1,12 @@
+﻿using Dhaf.Core;
+
+namespace Dhaf.HealthCheckers.Tcp
+{
+    public class Config : IHealthCheckerConfig
+    {
+        public string ExtensionName => "tcp";
+
+        public int? Port { get; set; }
+        public int? ReceiveTimeout { get; set; }
+    }
+}
---
+++ src/Dhaf.HealthCheckers.Tcp/InternalConfig.cs
@@ -0,0 +1,13 @@
+﻿using Dhaf.Core;
+
+namespace Dhaf.HealthCheckers.Tcp
+{
+    public class InternalConfig : IHealthCheckerInternalConfig
+    {
+        public string ExtensionName => "tcp";
+
+        public int DefReceiveTimeout { get; set; }
+        public int MinReceiveTimeout { get; set; }
+        public int MaxReceiveTimeout { get; set; }
+    }
+}
